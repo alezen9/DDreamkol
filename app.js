57fe742ac913fd963211d35cf67293097937eca2
alezen9 <aj0715@live.com>
2018-01-26 04:55:54 +0100

    init with server.js and app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+//var userConroller = require('./user/userController');
+//app.use('/users',userConroller);
+
+// to make it visible to the rest of the programm
+module.exports = app;
