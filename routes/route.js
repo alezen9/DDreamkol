2fec23e6c43f4ba1dfa3acb6e54ba9b978184d71
Aleksandar Gjoreski <aj0715@live.com>
2018-01-29 19:16:54 +0100

    upload working properly with file renaim based on village name entered adding unique id qith uniqid()

--- routes/route.js
+++ routes/route.js
@@ -6,8 +6,13 @@ let formidable = require('formidable');
 let pug = require('pug');
 let util = require('util');
 let fsExtra  = require('fs-extra');
+let uniqid = require('uniqid');
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({extended: true}));
+
 
 var new_location = __dirname + '/upload/';
+var village = "";
 
 router.get("/", (req, res) => {
     res.render("homepage");
@@ -18,6 +23,8 @@ router.get("/upload", (req, res) => {
 });
 
 router.post('/upload', function (req, res) {
+  //village = req.body.village;
+  //console.log(village);
   var form = new formidable.IncomingForm();
   /*var village = req.body.village;
   console.log(village);
@@ -29,14 +36,24 @@ router.post('/upload', function (req, res) {
   form.parse(req, function (err, fields, files){
     console.log('trying to upload');
     console.log('upload recived');
+    //console.log(village);
     //res.end(util.inspect({fields: fields, files: files}));
+  });
+
+  form.on('field', function(name, field) {
+    console.log('Got a field:', field);
+    console.log('Got a field name:', name);
+    village = field;
+    console.log('village: ',village)
+    //console.log(field.name);
   });
 
       /* this is where the renaming happens */
   form.on ('fileBegin', function(name, file){
+        console.log('filebegin here');
         //rename the incoming file to the file's name
-        file.path = new_location + file.name;
-})
+        file.path = new_location + uniqid(village + "-") + file.name;
+});
 
   form.on('end', function(fields, files) {
             console.log("success!");
