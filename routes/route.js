0a4ae6342856fdc3abb9760c4e57759424367b83
Aleksandar Gjoreski <aj0715@live.com>
2018-01-30 10:54:45 +0100

    pic pages added e piccole migliorie su homepage e upload

--- routes/route.js
+++ routes/route.js
@@ -10,7 +10,7 @@ let uniqid = require('uniqid');
 var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({extended: true}));
 
-
+// json vreating or reading with pic/file number
 var file_a;
 var exists = fs.existsSync('data.json');
 if (exists) {
@@ -27,35 +27,84 @@ if (exists) {
 			};
 }
 
-
+// global variables
 var new_location = __dirname + '/upload/';
 var village = "";
 var right_loc;
 
+
+//route homepage
 router.get("/", (req, res) => {
     res.render("homepage");
 });
 
+
+//route upload page
 router.get("/upload", (req, res) => {
   res.render("upload");
 });
 
+//route nerezi_pic page
+router.get("/nerezi_pic", (req, res) => {
+  res.render("nerezi_pic");
+});
+
+//route modric_pic page
+router.get("/modric_pic", (req, res) => {
+  res.render("modric_pic");
+});
+
+//route bezevo page
+router.get("/bezevo_pic", (req, res) => {
+  res.render("bezevo_pic");
+});
+
+//route borovec_pic page
+router.get("/borovec_pic", (req, res) => {
+  res.render("borovec_pic");
+});
+
+//route d_lukovo_pic page
+router.get("/d_lukovo_pic", (req, res) => {
+  res.render("d_lukovo_pic");
+});
+
+//route g_lukovo_pic page
+router.get("/g_lukovo_pic", (req, res) => {
+  res.render("g_lukovo_pic");
+});
+
+//route drenok_pic page
+router.get("/drenok_pic", (req, res) => {
+  res.render("drenok_pic");
+});
+
+//route jablanica_pic page
+router.get("/jablanica_pic", (req, res) => {
+  res.render("jablanica_pic");
+});
+
+//route lakavica_pic page
+router.get("/lakavica_pic", (req, res) => {
+  res.render("lakavica_pic");
+});
+
+//route piskupshtina_pic page
+router.get("/piskupshtina_pic", (req, res) => {
+  res.render("piskupshtina_pic");
+});
+
+
+//handling upload page form
 router.post('/upload', function (req, res) {
   //village = req.body.village;
   //console.log(village);
   var form = new formidable.IncomingForm();
-  /*var village = req.body.village;
-  console.log(village);
-  //form.uploadDir = path.join(__dirname, "upload");
-  var fileType = path.extname(req.body.file_upload);
-  console.log(fileType);
-  */
 
   form.parse(req, function (err, fields, files){
     console.log('trying to upload');
     console.log('upload recived');
     //console.log(village);
-    //res.end(util.inspect({fields: fields, files: files}));
   });
 
   form.on('field', function(name, field) {
