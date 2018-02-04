36a08d156a01648cdf9b0efdc8fcec750ff5982d
Aleksandar Gjoreski <aj0715@live.com>
2018-02-04 02:53:26 +0100

    homepage,upload page and pictures pages are mobile responsive now

--- public/js/scripts/footer.js
+++ public/js/scripts/footer.js
@@ -6,14 +6,16 @@ var footer = '\
                     <h3 class="text-white">Share it!</h3>\
                 </div>\
                 <div class="share d-table ml-auto mr-auto mt-5">\
-                    <a href="https://twitter.com/intent/tweet?url=https://fu-prod-storage.s3.amazonaws.com/content/hives/attachments/hive-1033/post-14441/Test654x234.png" class="btn btn-pill btn-primary mr-4" title="Share Shards on Twitter">\
-                        <i class="fa fa-twitter mr-1"></i>\
-                        Share on Twitter\
+                    <a style="margin-left: 1.5rem;" href="https://goo.gl/forms/vFWb1KEqR7bV8g4H3" class="btn btn-pill btn-primary mr-4" title="Share Shards on Twitter">\
+                        <i class="fa fa-comment mr-1">&ensp;</i>\
+                        Your Feedbak&ensp;\
                     </a>\
+                    <!--\
                     <a href="https://www.facebook.com/sharer/sharer.php?u=https://fu-prod-storage.s3.amazonaws.com/content/hives/attachments/hive-1033/post-14441/Test654x234.png" class="btn btn-pill btn-primary mr-4" title="Share on Facebook">\
                         <i class="fa fa-facebook mr-1"></i>\
                         Share on Facebook\
                     </a>\
+                    -->\
                 </div>\
             </div>\
             <p class="text-muted text-center small p-0 mb-4">&copy; Copyright 2018 — Aleksandar Gjoreski</p>\
