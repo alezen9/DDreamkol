276ee98af49350508f9717b05993bb1e780d65fb
Aleksandar Gjoreski <aj0715@live.com>
2018-05-25 19:16:55 +0200

    navbar and other small changes

--- public/js/scripts/navbar2.js
+++ public/js/scripts/navbar2.js
@@ -1,9 +1,9 @@
 var navbar2 = '\
 	<div>\
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">\
+		<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: rgba(0,0,0,0.8);">\
 			<a href="homepage.html"><img src="images/demo/shards-logo.svg" alt="Example Navbar 1" class="mr-2" height="30px"></a>\
 	        <a class="navbar-brand" style="margin-right: inherit; margin-left: 1rem;" href="/">DDreamKol</a>\
-	        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown-2" aria-controls="navbarNavDropdown-2" aria-expanded="false" aria-label="Toggle navigation">\
+	        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown-2" aria-controls="navbarNavDropdown-2" aria-expanded="false" aria-label="Toggle navigation" style="border: none;">\
 	            <span class="navbar-toggler-icon"></span>\
 	        </button>\
 	        <div class="collapse navbar-collapse mr-auto" id="navbarNavDropdown-2">\
